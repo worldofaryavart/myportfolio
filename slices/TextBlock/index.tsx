6e20119f62296e03e69104c480123a811a1ad763
Shiva Saini <factwiths@gmail.com>
2024-03-14 21:20:23 +0530

    first commit

---
+++ slices/TextBlock/index.tsx
@@ -0,0 +1,26 @@
+import Bounded from "@/components/Bounded";
+import { Content } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `TextBlock`.
+ */
+export type TextBlockProps = SliceComponentProps<Content.TextBlockSlice>;
+
+/**
+ * Component for "TextBlock" Slices.
+ */
+const TextBlock = ({ slice }: TextBlockProps): JSX.Element => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <div className="max-w-prose">
+      <PrismicRichText field={slice.primary.text} />
+    </div>
+    </Bounded>
+  );
+};
+
+export default TextBlock;
