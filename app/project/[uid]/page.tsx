30305cfad54d338ef56b703bdc28c5f6ea3b76ec
Shiva Saini <factwiths@gmail.com>
2024-03-19 12:41:39 +0530

    project page changes

--- app/project/[uid]/page.tsx
+++ app/project/[uid]/page.tsx
@@ -4,6 +4,7 @@ import { SliceZone } from "@prismicio/react";
 
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
+import ContentBody from "@/components/ContentBody";
 
 type Params = { uid: string };
 
@@ -13,7 +14,8 @@ export default async function Page({ params }: { params: Params }) {
     .getByUID("project", params.uid)
     .catch(() => notFound());
 
-  return <SliceZone slices={page.data.slices} components={components} />;
+  // return <SliceZone slices={page.data.slices} components={components} />;
+  return <ContentBody page = {page}/>;
 }
 
 export async function generateMetadata({
